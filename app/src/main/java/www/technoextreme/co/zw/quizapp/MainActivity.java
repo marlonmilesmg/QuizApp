6d5a4bdb5f6f12500a286f7b6e2a2952a53540a6
Marlon Magonjo <Seven@1Seven@2>
2020-03-26 22:52:17 +0200

    Let's Go!!!

---
+++ app/src/main/java/www/technoextreme/co/zw/quizapp/MainActivity.java
@@ -0,0 +1,14 @@
+package www.technoextreme.co.zw.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
